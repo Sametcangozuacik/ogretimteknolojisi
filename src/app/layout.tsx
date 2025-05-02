a34b196fb41e0ca5aaea971bf120de3609de8f24
Samet Can Gözüaçık <sametcanngozuacik@gmail.com>
2025-05-02 12:43:08 +0300

    feat: Update RootLayout structure and enhance mainsection styles with improved comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,20 +5,21 @@ import styles from "./globals.module.scss";
 // Font import
 const inter = Inter({ subsets: ["latin"] });
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <body className={`${styles.root} ${inter.className}`}>
-      <main>{children}</main>
-    </body>
-  );
-};
-
-export default RootLayout;
-
-// Metadata ve dinamik render yapılandırması
 export const metadata = {
   title: "Math Game",
   description: "A simple math game",
 };
 
 export const dynamic = "force-dynamic";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <html lang="tr">
+      <body className={`${styles.root} ${inter.className}`}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;

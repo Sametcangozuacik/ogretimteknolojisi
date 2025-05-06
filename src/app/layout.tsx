328f4d49a76bb82c967e8438de261817f84f8cbd
Samet Can Gözüaçık <sametcanngozuacik@gmail.com>
2025-05-06 16:12:36 +0300

    fix: Correct import path for Header component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import { Inter } from "next/font/google";
-import Header from "../components/header/Header";
+import Header from "../components/header/header";
 import styles from "./globals.module.scss";
 
 // Font import
